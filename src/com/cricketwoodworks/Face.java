fc79ab9b43d25ad4e1ebf0755fce2e08778379f4
Alan Rahlf <arahlf@gmail.com>
2012-01-22 11:06:55 -0600

    Adding back in ordinal/value statuses to the Face enum for simplicity.

--- src/com/cricketwoodworks/Face.java
+++ src/com/cricketwoodworks/Face.java
@@ -2,27 +2,39 @@ package com.cricketwoodworks;
 
 public enum Face {
     
-    ACE("Ace"),
-    TWO("Two"),
-    THREE("Three"),
-    FOUR("Four"),
-    FIVE("Five"),
-    SIX("Six"),
-    SEVEN("Seven"),
-    EIGHT("Eight"),
-    NINE("Nine"),
-    TEN("Ten"),
-    JACK("Jack"),
-    QUEEN("Queen"),
-    KING("King");
+    ACE("Ace", 1, 1),
+    TWO("Two", 2, 2),
+    THREE("Three", 3, 3),
+    FOUR("Four", 4, 4),
+    FIVE("Five", 5, 5),
+    SIX("Six", 6, 6),
+    SEVEN("Seven", 7, 7),
+    EIGHT("Eight", 8, 8),
+    NINE("Nine", 9, 9),
+    TEN("Ten", 10 , 10),
+    JACK("Jack", 11, 10),
+    QUEEN("Queen", 12, 10),
+    KING("King", 13, 10);
     
     public String getDisplayName() {
         return _displayName;
     }
     
-    private Face(String displayName) {
-        _displayName = displayName;
+    public int getOrdinal() {
+        return _ordinal;
     }
     
+    public int getValue() {
+        return _value;
+    }
+    
+    private Face(String displayName, int ordinal, int value) {
+        _displayName = displayName;
+        _ordinal = ordinal;
+        _value = value;
+    }
+    
+    private final int _ordinal;
+    private final int _value;
     private final String _displayName;
 }
