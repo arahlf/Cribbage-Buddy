a47abb9a054249644c5cfe5736b1af05890fe957
Alan Rahlf <arahlf@gmail.com>
2012-01-22 11:26:27 -0600

    Adding unversioned files.

---
+++ src/com/cricketwoodworks/cribbage/rules/hand/CribbagHandScoreRules.java
@@ -0,0 +1,31 @@
+package com.cricketwoodworks.cribbage.rules.hand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cricketwoodworks.cribbage.Score;
+import com.cricketwoodworks.cribbage.hands.Hand;
+
+public class CribbagHandScoreRules implements HandScoringRule {
+    
+    public CribbagHandScoreRules() {
+        _rules.add(new Flush());
+        _rules.add(new Multiple());
+        _rules.add(new Nobs());
+        _rules.add(new Run());
+        _rules.add(new FifteenCount());
+    }
+    
+    @Override
+    public List<Score> scoreHand(Hand hand) {
+        List<Score> scores = new ArrayList<Score>();
+        
+        for (HandScoringRule rule : _rules) {
+            scores.addAll(rule.scoreHand(hand));
+        }
+        
+        return scores;
+    }
+    
+    private final List<HandScoringRule> _rules = new ArrayList<HandScoringRule>();
+}
