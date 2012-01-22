f90dbee4f10b5ef84c63e4a1479f0f99d8207ea0
Alan Rahlf <arahlf@gmail.com>
2012-01-22 15:47:29 -0600

    Adding a few more unit tests.

--- test/src/com/arahlf/cribbage/rules/hand/TU_FifteenCount.java
+++ test/src/com/arahlf/cribbage/rules/hand/TU_FifteenCount.java
@@ -36,5 +36,30 @@ public class TU_FifteenCount extends AbstractRuleTestCase {
         assertScore(scores.get(0), 2, "Fifteen for 2", 2);
     }
     
+    public void testThreeFives() {
+        Hand hand = new Hand(FIVE_OF_HEARTS, FIVE_OF_DIAMONDS, FIVE_OF_SPADES, EIGHT_OF_DIAMONDS, THREE_OF_CLUBS);
+        
+        List<Score> scores = _rule.scoreHand(hand);
+        
+        assertEquals(1, scores.size());
+        assertScore(scores.get(0), 2, "Fifteen for 2", 3);
+    }
+    
+    public void testFourFives() {
+        Hand hand = new Hand(FIVE_OF_HEARTS, FIVE_OF_DIAMONDS, FIVE_OF_SPADES, FIVE_OF_CLUBS, THREE_OF_CLUBS);
+        
+        List<Score> scores = _rule.scoreHand(hand);
+        
+        assertEquals(4, scores.size());
+    }
+    
+    public void testPerfectHand() {
+        Hand hand = new Hand(FIVE_OF_HEARTS, FIVE_OF_DIAMONDS, FIVE_OF_SPADES, FIVE_OF_CLUBS, JACK_OF_HEARTS);
+        
+        List<Score> scores = _rule.scoreHand(hand);
+        
+        assertEquals(8, scores.size());
+    }
+    
     private HandScoringRule _rule = new FifteenCount();
 }
